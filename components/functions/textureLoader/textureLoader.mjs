e592aafb20b20c7b81a738a514643abfd37c095a
Spartakus <vonerikbarren-tucker@VonEriks-MacBook-Pro.local>
2023-06-29 11:13:00 -0400

    adding components dir

---
+++ components/functions/textureLoader/textureLoader.mjs
@@ -0,0 +1,9 @@
+import * as THREE from '../../../libraries/modules/three.module.js';
+
+
+// Import TextureLoader
+let loadMessage = "addingMessage"
+let textureLoader = new THREE.TextureLoader();
+
+
+// console.log(loadMessage + "\n" + textureLoader)
